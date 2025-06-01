d11a818526d28056aa5001aa492496e26136e01d
Tshims <manassetshims@gmail.com>
2025-06-01 01:48:41 +0100

    +++ Route/web.php pour le reset du password

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\Auth\ResetPasswordController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-mail', function () {
+    Mail::raw('Ceci est un test depuis Laravel !', function ($message) {
+        $message->to('manassetshims@gmail.com')
+                ->subject('Test de Laravel');
+    });
+
+    return 'Mail envoyé !';
+});
+
+Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('/reset-password', [ResetPasswordController::class, 'reset'])->name('password.update');
+
